4874e9c92393dfb93924c9f8a0a8fbe9c3127081
Andrea <you@example.com>
2023-02-21 10:02:00 +0100

    añadir footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+    <footer class="footer">
+        <h3> Create for Andrea Valencia in 2023 ©</h3>
+    </footer>
+
+</template>
+
+
+<script setup></script>
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,7 +13,9 @@
             <div class="divCard">
               <TaskItem v-for="task in tasks" :key="task.id" :task="task" />
           </div>
+          <Footer />
       </div>
+      
     <!-- </div> -->
 </template>
 
@@ -24,6 +26,7 @@ import { useRouter } from 'vue-router';
 import Nav from '../components/Nav.vue';
 import NewTask from '../components/NewTask.vue';
 import TaskItem from '../components/TaskItem.vue';
+import Footer from '../components/Footer.vue';
 
 const taskStore = useTaskStore();
 
