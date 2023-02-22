a5f78fcd06b57fb0e3aac08a72fcbaffcbf58615
Andrea <you@example.com>
2023-02-22 17:38:05 +0100

    prueba funcional

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -67,6 +67,20 @@
         </div>
       </div>
     </div>
+    <div v-if="modalWelcome" class="overlay">
+          <div class="modalWelcome">
+              <h3> Welcome </h3>
+              <div class="centerImg">
+                  <img class="catImgTwo" src="../images/welcomeCat.gif"/>
+              </div>
+              <p> ¡You completed your first task, enjoy your work! </p>
+              <div>
+                  <button @click="closeModal" class="modalButtonTwo">  
+                      <img src="../images/closeButton.svg"/>
+                  </button>
+              </div>
+          </div>
+    </div>
     </form>
     </div>
   </div>
@@ -83,6 +97,9 @@ import { useRouter } from "vue-router";
 import { useUserStore } from "../stores/user";
 import { storeToRefs } from "pinia";
 
+//guardamos en una variable el valor booleano para que no se enseñe el modal
+const modalWelcome = ref (false);
+
 // Route Variables
 const route = "/auth/login";
 const buttonText = "Sign In";
@@ -98,14 +115,26 @@ const errorMsg = ref("");
 // Router to push user once SignedUp to Log In
 const redirect = useRouter();
 
+//función para cerrar el modal al hacer click en delete
+const closeModal = () => {
+  modalWelcome.value = false;
+  document.body.style.overflow = 'auto'
+}
+
 // Arrow function to SignUp user to supaBase with a timeOut() method for showing the error
 const signUp = async () => {
   if (password.value === confirmPassword.value) {
     try {
       // calls the user store and send the users info to backend to logIn
       await useUserStore().signUp(email.value, password.value);
-      // redirects user to the homeView
-      redirect.push({ path: "/auth/login" });
+
+      //se le vuelve a dar un valor a la variable booleanica para que ahora enseñe el contenido
+      modalWelcome.value = true; 
+      setTimeout(() => {
+        // redirects user to the homeView
+        redirect.push({ path: "/auth/login" });
+      }, 6000); 
+      
     } catch (error) {
       // displays error message
       errorMsg.value = error.message;
